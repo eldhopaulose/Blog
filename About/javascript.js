c6a35e8f2e4a450a8f0f64e95aa7b783ae7586ba
eldhopaulose <eldhopaulose0485@gmail.com>
2020-08-02 22:34:20 +0530

    status bar seting

---
+++ About/javascript.js
@@ -0,0 +1,25 @@
+window.onscroll = function() {
+    move();
+};
+var i = 0;
+
+function move() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("myBar");
+        var width = 1;
+        var id = setInterval(frame, 30);
+
+        function frame() {
+            if (width >= 80) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+                document.getElementById("myBar").innerHTML = width++ + "%";
+            }
+        }
+    }
+}
+s
