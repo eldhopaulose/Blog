df165cd43b58d9f2b33a71b46dc66ca08602a447
eldhopaulose <eldhopaulose0485@gmail.com>
2020-08-03 22:56:24 +0530

    PWA notification2

--- Service_Worke.js
+++ Service_Worke.js
@@ -21,21 +21,3 @@ if ("serviceWorker" in navigator) {
 } else {
     alert("Service workers are not supported.");
 }
-
-
-
-
-function showNotification() {
-    Notification.requestPermission(function(result) {
-        if (result === 'granted') {
-            navigator.serviceWorker.ready.then(function(registration) {
-                registration.showNotification('Vibration Sample', {
-                    body: 'Buzz! Buzz!',
-                    icon: '../images/touch/chrome-touch-icon-192x192.png',
-                    vibrate: [200, 100, 200, 100, 200, 100, 200],
-                    tag: 'vibration-sample'
-                });
-            });
-        }
-    });
-}
--- script.js
+++ script.js
@@ -56,3 +56,21 @@ window.onload = function() {
     css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #ffffff}";
     document.body.appendChild(css);
 };
+
+
+navigator.serviceWorker.register('sw.js');
+
+function showNotification() {
+    Notification.requestPermission(function(result) {
+        if (result === 'granted') {
+            navigator.serviceWorker.ready.then(function(registration) {
+                registration.showNotification('Vibration Sample', {
+                    body: 'Buzz! Buzz!',
+                    icon: '../images/touch/chrome-touch-icon-192x192.png',
+                    vibrate: [200, 100, 200, 100, 200, 100, 200],
+                    tag: 'vibration-sample'
+                });
+            });
+        }
+    });
+}
