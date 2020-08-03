756a570475b07b05d734913e454bae7da68ee4b6
eldhopaulose <eldhopaulose0485@gmail.com>
2020-08-03 22:41:10 +0530

    PWA12

--- sw.js
+++ sw.js
@@ -22,42 +22,6 @@ self.addEventListener('install', function(event) {
     );
 });
 
-
-
-
-
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent the mini-infobar from appearing on mobile
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI notify the user they can install the PWA
-    function showInstallPromotion() {
-        document.getElementById("btn")
-    }
-    showInstallPromotion();
-});
-
-
-
-buttonInstall.addEventListener('click', (e) => {
-    // Hide the app provided install promotion
-    hideMyInstallPromotion();
-    // Show the install prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the install prompt');
-        } else {
-            console.log('User dismissed the install prompt');
-        }
-    });
-});
-
-
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.open('mysite-dynamic').then(function(cache) {
@@ -69,45 +33,4 @@ self.addEventListener('fetch', function(event) {
             });
         })
     );
-});
-
-
-
-
-
-
-
-window.addEventListener('appinstalled', (evt) => {
-    // Log install to analytics
-    console.log('INSTALL: Success');
-});
-
-
-
-
-
-window.addEventListener('DOMContentLoaded', () => {
-    let displayMode = 'browser tab';
-    if (navigator.standalone) {
-        displayMode = 'standalone-ios';
-    }
-    if (window.matchMedia('(display-mode: standalone)').matches) {
-        displayMode = 'standalone';
-    }
-    // Log launch display mode to analytics
-    console.log('DISPLAY_MODE_LAUNCH:', displayMode);
-});
-
-
-
-
-window.addEventListener('DOMContentLoaded', () => {
-    window.matchMedia('(display-mode: standalone)').addListener((evt) => {
-        let displayMode = 'browser tab';
-        if (evt.matches) {
-            displayMode = 'standalone';
-        }
-        // Log display mode change to analytics
-        console.log('DISPLAY_MODE_CHANGED', displayMode);
-    });
 });
