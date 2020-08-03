6f024d71603a342453be505a1ca14a242ab73174
eldhopaulose <eldhopaulose0485@gmail.com>
2020-08-03 20:27:05 +0530

    PWA8

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-self.addEventListener("install", (e) => {
+self.addEventListener("install", e => {
     console.log("[Service Worker] Install");
     e.waitUntil(
-        caches.open("cacheName").then((cache) => {
+        caches.open("static").then(cache => {
             return cache.addAll([
                 "./",
                 "./style/media.css",
@@ -16,15 +16,15 @@ self.addEventListener("install", (e) => {
                 "./About/style/style.css",
                 "./About/About.html",
                 "./About/javascript.js",
-            ]);
+            ])
         })
-    );
-});
+    )
+})
 
 self.addEventListener("fetch", e => {
     e.respondWith(
         caches.match(e.request).then(response => {
-            return response || fetch(e.request);
+            return response || fetch(e.request)
         })
-    );
-});
+    )
+})
