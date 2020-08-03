2dafbefab05775bb6fc9ce28edd3463b7a1c882f
eldhopaulose <eldhopaulose0485@gmail.com>
2020-08-03 21:00:31 +0530

    PWA9

--- sw.js
+++ sw.js
@@ -1,30 +1,37 @@
-self.addEventListener("install", e => {
-    console.log("[Service Worker] Install");
-    e.waitUntil(
-        caches.open("static").then(cache => {
-            return cache.addAll([
-                "./",
-                "./style/media.css",
-                "./script.js",
-                "./style/style.css",
-                "./log/64x64.png",
-                "./log/512x512.png",
-                "./images/unnamed.png",
-                "./About/images/IMG_1085.jpg",
-                "./About/images/unnamed.png",
-                "./About/style/media.css",
-                "./About/style/style.css",
-                "./About/About.html",
-                "./About/javascript.js",
-            ])
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(
+                [
+                    "./",
+                    "./style/media.css",
+                    "./script.js",
+                    "./style/style.css",
+                    "./log/64x64.png",
+                    "./log/512x512.png",
+                    "./images/unnamed.png",
+                    "./About/images/IMG_1085.jpg",
+                    "./About/images/unnamed.png",
+                    "./About/style/media.css",
+                    "./About/style/style.css",
+                    "./About/About.html",
+                    "./About/javascript.js",
+                ]
+            );
         })
-    )
-})
+    );
+});
 
-self.addEventListener("fetch", e => {
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request)
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.open('mysite-dynamic').then(function(cache) {
+            return cache.match(event.request).then(function(response) {
+                return response || fetch(event.request).then(function(response) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
         })
-    )
-})
+    );
+});
