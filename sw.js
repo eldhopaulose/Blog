651fbc0c4f68f1478bbb91086b27c9c9b30a3269
eldhopaulose <eldhopaulose0485@gmail.com>
2020-08-03 21:35:13 +0530

    PWA10

--- sw.js
+++ sw.js
@@ -21,6 +21,20 @@ self.addEventListener('install', function(event) {
         })
     );
 });
+buttonInstall.addEventListener('click', (e) => {
+    // Hide the app provided install promotion
+    hideMyInstallPromotion();
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the install prompt');
+        } else {
+            console.log('User dismissed the install prompt');
+        }
+    });
+});
 
 
 self.addEventListener('fetch', function(event) {
