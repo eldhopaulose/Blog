bef5454e8e45763f64037f31847f901f1faa04bf
eldhopaulose <eldhopaulose0485@gmail.com>
2020-08-03 23:43:41 +0530

    PWA notification

--- sw.js
+++ sw.js
@@ -34,3 +34,23 @@ self.addEventListener('fetch', function(event) {
         })
     );
 });
+
+
+let promptEvent = undefined;
+self.addEventListener('beforeinstallprompt', event => {
+    promptEvent = event;
+    $("installbuttin").removeClass("d-none");
+    $("installbuttin").addClass("d-block");
+});
+
+// if (promptEvent) {
+//     $("installbuttin").removeClass("d-none");
+//     $("installbuttin").addClass("d-block");
+// } else {
+//     $("installbuttin").removeClass("d-block");
+//     $("installbuttin").addClass("d-none");
+// }
+
+$("#installbuttin").click(function() {
+    promptEvent.prompt();
+});
