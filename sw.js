f43e2c0ec0eea26840645f43fa67d2408af0cb62
eldhopaulose <eldhopaulose0485@gmail.com>
2020-08-03 20:16:17 +0530

    PWA7

--- sw.js
+++ sw.js
@@ -21,9 +21,9 @@ self.addEventListener("install", (e) => {
     );
 });
 
-self.addEventListener("fetch", (e) => {
+self.addEventListener("fetch", e => {
     e.respondWith(
-        caches.match(e.request).then(Response => {
+        caches.match(e.request).then(response => {
             return response || fetch(e.request);
         })
     );
